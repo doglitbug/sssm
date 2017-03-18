6b1ee96f8acb398fb40bd5001cb395edf53f5f6f
Arron Dick <s_drac2@yahoo.com>
2017-03-18 20:57:21 +1300

    Completed log in with checks and error messages

--- public/user/login.php
+++ public/user/login.php
@@ -9,7 +9,7 @@ $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 $username = "";
 
 //Set error vars
-$login_error = "";
+$username_error = $password_error = $login_error = "";
 
 //Check to see if form has been submitted
 if (isset($_POST['submit'])){
@@ -17,8 +17,35 @@ if (isset($_POST['submit'])){
 	$username  = mysqli_real_escape_string($dbc, trim($_POST['username']));
 	$password = mysqli_real_escape_string($dbc, trim($_POST['password']));
 
-	//TODO Log in user
+	//Check username has been enetered
+	if (empty($username)){
+		$username_error="Please enter a username";
+	} elseif (empty($password)){
+		$password_error = "Please enter your password";
+	} else {
+		//Username and password have been entered
+		//Build query
+		$query = "SELECT * FROM tbl_user WHERE username='$username' LIMIT 1";
+		$result = mysqli_query($dbc, $query) or die('Couldn\'t search for user: ') . mysqli_error($dbc);
+		//Grab rows
+		$row = mysqli_fetch_array($result);
 
+		//Check if username was found, then that the password is correct
+		if (mysqli_num_rows($result)!=0 && (password_verify($password, $row['password']))){
+			//Log in user
+			$_SESSION['id']=$row['id'];
+			$_SESSION['username']=$row['username'];
+			$_SESSION['firstname']=$row['firstname'];
+			$_SESSION['manager']=$row['manager'];
+
+			//TODO redirect user
+			echo "<p>Login correct</p>";
+			require_once('../scripts/footer.php');
+			die();
+		} else {
+			$login_error="Username and/or password invalid, please try again";
+		}
+	}
 }
 
 ?>
@@ -28,11 +55,13 @@ if (isset($_POST['submit'])){
 	<div class="form-group">
 		<label for="username">Username</label>
 		<input type="text" class="form-control" id="username" placeholder="Username" name="username"/>
+		<div class="error"><?php echo $username_error;?></div>		
 	</div>
 
 	<div class="form-group">
 		<label for="password">Password</label>
 		<input type="password" class="form-control" id="password" placeholder="Password" name="password"/>
+		<div class="error"><?php echo $password_error;?></div>	
 	</div>
 
 	<div class="error"><?php echo $login_error;?></div>
