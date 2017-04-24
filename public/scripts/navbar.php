d7ce5cd6dd8475e3624125d3991e992c0b1bd52e
Arron Dick <s_drac2@yahoo.com>
2017-04-24 15:44:02 +1200

    Added much more to navbar, need to add blank stubs for other pages

--- public/scripts/navbar.php
+++ public/scripts/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-navbar" aria-expanded="false">
@@ -7,15 +7,42 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 	      	</button>
-	      	<a class="navbar-brand" href="#">SSSM</a>
+	      	<a class="navbar-brand" href="../index.php">SSSM</a>
 	      </div>
 
 	     <div class="collapse navbar-collapse" id="bs-navbar">
 	     	<ul class="nav navbar-nav">
-	     		<li class="active"><a href="#">Link active</a></li>
-	     		<li><a href="#">Link</a></li>
+	     	<?php
+	     	//Users links, manager only
+	      	if (isset($_SESSION['manager']) && $_SESSION['manager']=='1'){
+	      		?>
+	      		<li class="dropdown">
+	      			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">User management<span class="caret"></span></a>
+	      			<ul class="dropdown-menu">
+	      				<li><a href="../user/new.php">New</a></li>
+	      				<li><a href="../user/modify.php">Modify</a></li>
+	      				<li><a href="../user/delete.php">Delete</a></li>
+	      			</ul>
+	      		</li>
+	      	<?php
+	      	}
+	     	?>
+	      		<li><a href="../schedule/view.php">Availability</a></li>
+	     		<li><a href="../roster/view.php">Roster</a></li>
+	     	</ul>
+
+	     	<!--log/profile links -->
+	     	<ul class="nav navbar-nav navbar-right">
+        		<li class="dropdown">
+          			<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Myself<span class="caret"></span></a>
+          			<ul class="dropdown-menu">
+          			<!-- TODO Hide unneeded links, eg no log in if already logged in -->
+	     				<li><a href="../user/login.php">Login</a></li>
+	     				<li><a href="../user/modify.php">Edit my details</a></li>
+	     				<li><a href="../user/logout.php">Logout</a></li>
+	     			</ul>
+	     		</li>
 	     	</ul>
 	     </div>
-
     </div>
 </nav>
